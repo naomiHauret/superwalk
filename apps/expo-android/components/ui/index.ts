e61feff33bffa70991c81a412e40e99c355c325e
naomiHauret <hauretnaomi@gmail.com>
2024-08-08 11:24:42 +0200

    chore: ui nativewind + rn primitives instead of tamagui

---
+++ apps/expo-android/components/ui/index.ts
@@ -0,0 +1,4 @@
+export * from './avatar'
+export * from './button'
+export * from './text'
+export * from './typography'
--- apps/expo-android/metro.config.js
+++ apps/expo-android/metro.config.js
@@ -1,6 +1,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config')
-
+const { withNativeWind } = require('nativewind/metro')
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
 
@@ -8,4 +8,4 @@ const config = getDefaultConfig(__dirname)
 config.resolver.unstable_enablePackageExports = true
 config.resolver.unstable_conditionNames = ['react-native', 'browser', 'require']
 
-module.exports = config
+module.exports = withNativeWind(config, { input: './globals.css' })
